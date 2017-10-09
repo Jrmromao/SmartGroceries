e635869e91d8224ce0576f1620bc9511ae4dffd4
Joao Romao <jrmromao@gmail.com>
2017-10-09 22:04:53 +0100

    Initial commit

---
+++ app/src/main/java/co/devhut/smartgroceries/MainActivity.java
@@ -0,0 +1,85 @@
+package co.devhut.smartgroceries;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView registerLink = (TextView) findViewById(R.id.newAccountLink);
+        Button login_btn = (Button) findViewById(R.id.btn_login);
+//        TextView txt_username = (TextView) findViewById(R.id.input_username);
+//        TextView txt_password = (TextView) findViewById(R.id.input_password);
+        final ProgressBar load = (ProgressBar) findViewById(R.id.pb_loadProfile);
+
+       load.setVisibility(View.GONE);
+
+        //
+        //  create an arra of intends objects
+        //
+        final Intent[] intent = new Intent[2];
+
+
+
+
+
+
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(), "Register Working", Toast.LENGTH_SHORT).show();
+
+
+               }
+            } );
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Explicit Intent
+                intent[0] = new Intent(getApplicationContext(), Main2Activity.class);
+                intent[1] = new Intent(getApplicationContext(), Scan_Activity.class);
+
+                try{
+
+                    startActivity(intent[0]);
+
+                }catch (Exception e){
+                    Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
+                }
+
+
+              //  loadProgressBar(load);
+            }
+        } );
+
+    }
+
+
+
+
+        public void loadProgressBar(ProgressBar load){
+
+            load.setVisibility(View.VISIBLE);
+
+            load.setMax(5);
+
+
+    }
+
+
+
+
+}
---
+++ app/src/main/java/co/devhut/smartgroceries/Scan_Activity.java
@@ -0,0 +1,155 @@
+package co.devhut.smartgroceries;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+
+import java.security.Permissions;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+import static android.Manifest.permission_group.CAMERA;
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+
+public class Scan_Activity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+
+    private ZXingScannerView mScannerView;
+    private static final int REQUESR_CAMERA = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkPermission()) {
+                Toast.makeText(this, "Permission is granted", Toast.LENGTH_SHORT).show();
+            } else {
+                requestPermission();
+            }
+        }
+    }
+
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(this, new String[]{CAMERA}, REQUESR_CAMERA);
+    }
+
+    private boolean checkPermission()    {
+        return (ContextCompat.checkSelfPermission(Scan_Activity.this, Manifest.permission.CAMERA)==PackageManager.PERMISSION_GRANTED);
+    }
+
+    public void onRequestPermissionsResult(final int requestCode, String permission[], int grantResults[]){
+
+        switch (requestCode){
+            case REQUESR_CAMERA :
+                if(grantResults.length > 0){
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if(cameraAccepted){
+                        Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                    }
+               else{
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+
+                        if(shouldShowRequestPermissionRationale(CAMERA)){
+
+                            displayAlertMessage("You need to have access for both permission",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                requestPermissions(new String[]{CAMERA}, REQUESR_CAMERA);
+                                            }
+                                        }
+                                    });
+                            return;
+                        }
+                    }
+                }
+        }
+        break;
+    }
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(checkPermission())
+            {
+                if(mScannerView == null){
+                    mScannerView = new ZXingScannerView(this);
+                    setContentView(mScannerView);
+                }
+                mScannerView.setResultHandler(this);
+                mScannerView.startCamera();
+
+            }
+            else{
+                requestPermission();
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        mScannerView.stopCamera();
+    }
+
+    public void displayAlertMessage(String message , DialogInterface.OnClickListener listener){
+        new AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton("OK", (DialogInterface.OnClickListener) listener)
+                .setNegativeButton("Cancel", (DialogInterface.OnClickListener) listener)
+                .create()
+                .show();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+
+        final String scanResult = result.getText();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("scan result");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                mScannerView.resumeCameraPreview(Scan_Activity.this);
+            }
+        });
+
+        builder.setNeutralButton("Visit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(scanResult));
+                startActivity(intent);
+            }
+        });
+
+        builder.setMessage(scanResult);
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+
+}
